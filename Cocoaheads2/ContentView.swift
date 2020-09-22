bc5a3bff4d63257eb56f3a43c0d529049e8e2ccd
Christian Menschel <christian@tapwork.de>
2020-09-22 22:07:53 +0200

    Initial Commit

---
+++ Cocoaheads2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cocoaheads2
+//
+//  Created by Christian Menschel on 22.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
